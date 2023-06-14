7a8a5f78ea4eee5b46b0d2a572b1df879dd23480
jpmar327 <jpmartins327@gmail.com>
2023-06-13 21:31:34 -0700

    Formatted Code and Added Note

--- format_files.py
+++ format_files.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 # LOOK OUT FOR FILES LIKE .PNG, .JPG, .MOV, .NEF
+# ADD DELETE ORIGINAL FILE FUNCTION: VAL = DELETE_ORIG_YN
 count = 0
 files_prefix = 'Redwoods'
 month = '6'
@@ -15,39 +16,14 @@ print(len(os.listdir(fr"{folder_path}/{test_dir}")))
 
 
 for dirfolder in os.listdir(folder_path):
-    # print(dirfolder)
     dsc_tracker = ''
     for dirname in os.listdir(fr"{folder_path}/{dirfolder}"):
-        # print(dirname)
         if dsc_tracker == '' or  dirname.split('.')[0] != dsc_tracker:
             dsc_tracker = dirname.split('.')[0]
             count += 1
             print(dsc_tracker)
         file_type = dirname.split('.')[-1]
-        # if file_type == 'JPG':
-        #     print(file_type)
         old_path = fr"{folder_path}/{dirfolder}/{dirname}"
         new_path = fr"{folder_path}/{dirfolder}/{files_prefix}_{month}_{year}_{count}.{file_type}"
         new_path_dir = fr"C:/Users/jpmar/Pictures/DSLR/Test/{file_type}/{files_prefix}_{month}_{year}_{count}.{file_type}"
-        # # if dirname.split('.')[0] != dsc_tracker:
-        # #    dsc_tracker 
-        # # print(count,dsc_tracker,file_type,dirname)
-        # shutil.move(old_path, new_path_dir)
-        # # os.rename(old_path, new_path)
-        # # print(old_path, new_path)
-        
-        # shutil.move(new_path, new_path_dir)
-        
-        # print(fr"/{dirfolder}/{dirname}", fr'/{dirfolder}/{files_prefix}_{month}_{year}_{count}.{file_type}', fr"/{file_type}/{files_prefix}_{month}_{year}_{count}.{file_type}")
-        # print(f"{dirfolder.split('D3100')[0]}_{dirname} -- {files_prefix}_{month}_{year}_{count}.{file_type}")
-        # print(f"{dirname} -- {files_prefix}_{month}_{year}_{count}.{file_type}")
-    # if os.path.isdir(dirname):
-    #     for i, filename in enumerate(os.listdir(dirname)):
-    #         print(dirname)
-            # os.rename(dirname + "/" + filename, dirname + "/" + str(i) + ".bmp")
 print(count)
-
-# for dirfolder in os.listdir(folder_path):
-#     dsc_tracker = ''
-#     for dirname in os.listdir(fr"{folder_path}/{dirfolder}"):
-#         if dsc_tracker == '' or  dirname.split('.')[0] != dsc_tracker:
--- format_files_v2.py
+++ format_files_v2.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 # LOOK OUT FOR FILES LIKE .PNG, .JPG, .MOV, .NEF
-# count = 0
+# ADD DELETE ORIGINAL FILE FUNCTION: VAL = DELETE_ORIG_YN
 
 # ------------- CHANGE INPUTS BELOW ------------- 
 files_prefix = 'RW'
@@ -9,14 +9,6 @@ month = '6'
 year = '2023'
 folder_path = r"C:/Users/jpmar/Pictures/DSLR/Redwoods State Park - June 9 2023"
 # ------------------------------------------------
-
-# folder_path = r"C:/Users/jpmar/Pictures/DSLR/Test_Pics"
-subfolder = 'Formatted'
-# output_folder = fr"{folder_path}/Formatted"
-
-# dsc_list = []
-# dsc_folder_tracker = ''
-# dsc_tracker = ''
 
 def return_files_types(folder_path):
     ''' 
@@ -34,32 +26,11 @@ def create_file_types_folders(folder_path,subfolder,files_type_list):
         os.makedirs(os.path.dirname(new_folder_path), exist_ok=True)
 
 
-# def copy_rename_file_to_new_dir(old_path, copy_path, new_path):
-#     print(old_path, copy_path, new_path)
-#     shutil.copy(old_path,copy_path)
-    # os.rename(copy_path, new_path)
-
-# def assign_file_name(folder_path):
-#     count = 0
-#     dsc_tracker = ''
-
-#     for dirfolder in os.listdir(folder_path):
-#         for dirname in os.listdir(fr"{folder_path}/{dirfolder}"):
-#             file_name = dirname.split('.')[0]
-#             filepath = fr"{dirfolder}/{file_name}"
-#             if dsc_tracker == '' or filepath != dsc_tracker:
-#                 dsc_tracker = fr"{dirfolder}/{file_name}"
-#                 count += 1
-#             file_type = dirname.split('.')[-1]
-
 def format_files(folder_path,subfolder,files_prefix,month,year):
     count = 0
     dsc_tracker = ''
-    files_type_list = return_files_types(folder_path)
-    create_file_types_folders(folder_path,subfolder,files_type_list)
 
     for dirfolder in os.listdir(folder_path):
-        # if dirfolder == 'Formatted':
         if dirfolder == subfolder:
             break
         for dirname in os.listdir(fr"{folder_path}/{dirfolder}"):
@@ -71,36 +42,11 @@ def format_files(folder_path,subfolder,files_prefix,month,year):
             file_type = dirname.split('.')[-1]
 
             old_path = fr"{folder_path}/{dirfolder}/{dirname}"
-            # copy_path = fr"{folder_path}/{subfolder}/{file_type}/{dirname}"
             new_path = fr"{folder_path}/{subfolder}/{file_type}/{files_prefix}_{month}_{year}_{count}.{file_type}"
-            # copy_rename_file_to_new_dir(old_path, copy_path, new_path)
-            # print(old_path, copy_path, new_path)
-            print(old_path, new_path)
+            # print(old_path, new_path)
             shutil.copy(old_path,new_path)
-            # os.rename(copy_path, new_path)
-# #         new_path_dir = fr"C:/Users/jpmar/Pictures/DSLR/Test/{file_type}/{files_prefix}_{month}_{year}_{count}.{file_type}"
-# #         # # if dirname.split('.')[0] != dsc_tracker:
-# #         # #    dsc_tracker 
-# #         # # print(count,dsc_tracker,file_type,dirname)
-# #         # shutil.move(old_path, new_path_dir)
 
-# #         # # os.rename(old_path, new_path)
-# #         # # print(old_path, new_path)
-        
-# #         # shutil.move(new_path, new_path_dir)
-        
-# #         # print(fr"/{dirfolder}/{dirname}", fr'/{dirfolder}/{files_prefix}_{month}_{year}_{count}.{file_type}', fr"/{file_type}/{files_prefix}_{month}_{year}_{count}.{file_type}")
-# #         # print(f"{dirfolder.split('D3100')[0]}_{dirname} -- {files_prefix}_{month}_{year}_{count}.{file_type}")
-# #         # print(f"{dirname} -- {files_prefix}_{month}_{year}_{count}.{file_type}")
-# #     # if os.path.isdir(dirname):
-# #     #     for i, filename in enumerate(os.listdir(dirname)):
-# #     #         print(dirname)
-# #             # os.rename(dirname + "/" + filename, dirname + "/" + str(i) + ".bmp")
-# # print(count)
-
-# # # for dirfolder in os.listdir(folder_path):
-# # #     dsc_tracker = ''
-# # #     for dirname in os.listdir(fr"{folder_path}/{dirfolder}"):
-# # #         if dsc_tracker == '' or  dirname.split('.')[0] != dsc_tracker:
-
+subfolder = 'Formatted'
+files_type_list = return_files_types(folder_path)
+create_file_types_folders(folder_path,subfolder,files_type_list)
 format_files(folder_path,subfolder,files_prefix,month,year)
