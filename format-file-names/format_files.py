2b859acc76b03f886eac4a1f3c44744e2a41c715
jpmar327 <jpmartins327@gmail.com>
2026-03-19 17:09:41 -0400

    updates

--- format-file-names/format_files.py
+++ format-file-names/format_files.py
@@ -4,10 +4,10 @@ import shutil
 # ADD DELETE ORIGINAL FILE FUNCTION: VAL = DELETE_ORIG_YN
 
 # ------------- CHANGE INPUTS BELOW ------------- 
-files_prefix = 'RW'
-month = '6'
-year = '2023'
-folder_path = r"C:/Users/jpmar/Pictures/DSLR/Redwoods State Park - June 9 2023"
+files_prefix = 'JP_Film'
+month = '2'
+year = '2026'
+folder_path = r"C:\Users\jpmar\Pictures\Film\Test"
 # ------------------------------------------------
 
 def return_files_types(folder_path):
@@ -26,32 +26,42 @@ def create_file_types_folders(folder_path,subfolder,files_type_list):
         os.makedirs(os.path.dirname(new_folder_path), exist_ok=True)
 
 
-def format_files(folder_path,subfolder,files_prefix,month,year):
-    count = 0
-    dsc_tracker = ''
+def format_files(folder_path,subfolder,files_prefix,month,year,files_type_list):
+    for file_type_check in files_type_list:
+        count = 0
+        dsc_tracker = ''
+        for dirfolder in os.listdir(folder_path):
+            # print(dirfolder)
+            if dirfolder == subfolder:
+                break
+            for dirname in os.listdir(fr"{folder_path}/{dirfolder}"):
+                file_type = dirname.split('.')[-1]
+                if file_type_check.lower() == file_type.lower():
+                    print(dirname)
+                    file_name = dirname.split('.')[0]
+                    filepath = fr"{dirfolder}/{file_name}"
+                    if dsc_tracker == '' or filepath != dsc_tracker:
+                        dsc_tracker = fr"{dirfolder}/{file_name}"
+                        count += 1
+                        file_name_0_limit = 5
+                        file_count_str = int(file_name_0_limit-len(str(count)))*"0"+str(count)
+                    
 
-    for dirfolder in os.listdir(folder_path):
-        if dirfolder == subfolder:
-            break
-        for dirname in os.listdir(fr"{folder_path}/{dirfolder}"):
-            file_name = dirname.split('.')[0]
-            filepath = fr"{dirfolder}/{file_name}"
-            if dsc_tracker == '' or filepath != dsc_tracker:
-                dsc_tracker = fr"{dirfolder}/{file_name}"
-                count += 1
-            file_type = dirname.split('.')[-1]
-
-            old_path = fr"{folder_path}/{dirfolder}/{dirname}"
-            new_path = fr"{folder_path}/{subfolder}/{file_type}/{files_prefix}_{month}_{year}_{count}.{file_type}"
-            # print(old_path, new_path)
-            shutil.copy(old_path,new_path)
+                    
+                    print(file_type)
+                    old_path = fr"{folder_path}/{dirfolder}/{dirname}"
+                    new_path = fr"{folder_path}/{subfolder}/{file_type}/{files_prefix}_{month}_{year}_{file_count_str}.{file_type}"
+                    print(new_path)
+                    # print(old_path, new_path)
+                    shutil.copy(old_path,new_path)
 
 def main():
     subfolder = 'Formatted'
     print(f'\nFORMATTING PHOTOS\nFOLDER PATH INPUT: "{folder_path}"\n')
     files_type_list = return_files_types(folder_path)
+    print(files_type_list)
     create_file_types_folders(folder_path,subfolder,files_type_list)
-    format_files(folder_path,subfolder,files_prefix,month,year)
+    format_files(folder_path,subfolder,files_prefix,month,year,files_type_list)
     print(f'DONE FORMATTING PHOTOS\nFOLDER PATH OUTPUT: "{folder_path}/{subfolder}"\n')
 
 if __name__ == "__main__":
---
+++ format-file-names/sandbox.py
@@ -0,0 +1,3 @@
+test = '0000'
+print(4*'0')
+print(len(test))
